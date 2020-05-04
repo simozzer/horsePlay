7cd2aea2bff9469098d4a8eb6687ee1bbb3a7f45
i049605 <SimonMoscrop@sap.com>
2020-05-04 13:07:05 +0100

    style

--- src/app/race-horse.spec.ts
+++ src/app/race-horse.spec.ts
@@ -2,6 +2,6 @@ import { RaceHorse } from './race-horse';
 
 describe('RaceHorse', () => {
   it('should create an instance', () => {
-    expect(new RaceHorse()).toBeTruthy();
+    expect(new RaceHorse('n')).toBeTruthy();
   });
 });
